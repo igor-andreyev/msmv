eb95ec1fb551cb78c03f2d39fb536a4f5b7aa16d
igor-andreyev <andreev-ig@ya.ru>
2016-04-25 13:52:13 +0600

    Implemented FishEye lens emulation

---
+++ FishEye.h
@@ -0,0 +1,36 @@
+//
+// Created by iandreyev on 25.04.16.
+//
+
+#ifndef MSMV_FRAMEWORK_FISHEYE_H
+#define MSMV_FRAMEWORK_FISHEYE_H
+
+#include "AbstractTransformation.hpp"
+
+class FishEye : public AbstractTransformation {
+public:
+    const static unsigned hash = str_hash("FishEye");
+
+    FishEye(std::vector<std::string> args) : AbstractTransformation(args) {
+        if(args.size() < 1)
+            throw po::required_option("Cx in FishEye");
+        if(args.size() < 2)
+            throw po::required_option("Cy in FishEye");
+        if(args.size() < 3)
+            throw po::required_option("K in FishEye");
+        m_cx = boost::lexical_cast<float>(args[0]);
+        m_cy = boost::lexical_cast<float>(args[1]);
+        m_k = boost::lexical_cast<float>(args[1]);
+    }
+
+protected:
+    virtual cv::Mat apply();
+
+private:
+    double m_cx, m_cy, m_k;
+
+};
+
+
+
+#endif //MSMV_FRAMEWORK_FISHEYE_H
